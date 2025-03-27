5e643913d8562b9bec341f03055fc2f0c9b38640
jngui1 <jngui1@student.gsu.edu>
2025-03-27 13:10:51 -0400

    Created unit tests for scenarios 5 & 6
    These unit tests simulate and evaluate the results of the following user activities:
    User generates a password and chooses to copy the password to the clipboard. User quits.
    User generates a password and chooses to save it to the associated account. User quits.
    
    A 'conftest.py' file is also added for teat configuration purposes.

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest, create_read
+
+@pytest.fixture()
+def app():
+    app = create_read.app
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
---
+++ tests/test_generate_copy_quit.py
@@ -0,0 +1,137 @@
+import unittest, database, random, string, create_read
+
+from pass_generator import PasswordGenerator
+
+from flask.testing import FlaskClient
+
+class TestGenerateCopyQuit(unittest.TestCase):
+    def test_unique_password(self):
+        for i in range(0, 20):
+            user_pass_object = PasswordGenerator(minlen=2, maxlen=2,
+                minlchars=0, minuchars=0, minnumbers=0, minschars=0)
+            user_pass = user_pass_object.generate()
+
+            user_id = database.create_user("test"+str(i), "test"+str(i)+"pass")
+
+            database.store_password(user_id, user_pass, "Test")
+
+        try:
+            for attempt in range(1, 10):
+                user_pass_object = PasswordGenerator(minlen=1, maxlen=1,
+                    minlchars=0, minuchars=0, minnumbers=0, minschars=0)
+                user_pass = user_pass_object.generate()
+
+                for password_entry in database.get_password_history():
+                    self.assertNotEqual(user_pass, password_entry["password"])
+
+        except AssertionError:
+            for del_password_entry in database.get_password_history():
+                database.delete_user(del_password_entry["user_id"])
+
+            database.delete_test_passwords()
+
+            self.assertNotEqual(user_pass, password_entry["password"])
+
+        else:
+            for del_password_entry in database.get_password_history():
+                database.delete_user(del_password_entry["user_id"])
+
+            database.delete_test_passwords()
+
+    def test_meets_requirements(self):
+        test_minlen = random.randint(1, 10)
+        test_maxlen = random.randint(test_minlen, 10)
+
+        test_minuchars = random.randint(test_minlen, test_maxlen)
+
+        test_minlchars = random.randint(test_minlen - test_minuchars,
+            test_maxlen - test_minuchars)
+        if (test_minlchars < 0):
+            test_minlchars = 0
+
+        test_minnumbers = random.randint(
+            test_minlen - test_minuchars - test_minlchars,
+            test_maxlen - test_minuchars - test_minlchars)
+        if (test_minnumbers < 0):
+            test_minnumbers = 0
+
+        test_minschars = random.randint(
+            test_minlen - test_minuchars - test_minlchars - test_minnumbers,
+            test_maxlen - test_minuchars - test_minlchars - test_minnumbers)
+        if (test_minschars < 0):
+            test_minschars = 0
+
+        test_excludeuchars = str()
+        test_excludelchars = str()
+        test_excludenumbers = str()
+        test_excludeschars = str()
+
+        for char in string.ascii_uppercase:
+            if (random.random() < 0.5):
+                test_excludeuchars += char
+
+        for char in string.ascii_lowercase:
+            if (random.random() < 0.5):
+                test_excludelchars += char
+
+        for char in string.digits:
+            if (random.random() < 0.5):
+                test_excludenumbers += char
+
+        special_chars = ["!", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".",
+            "-", "_", "+", "=", "<", ">", "?"]
+
+        for char in special_chars:
+            if (random.random() < 0.5):
+                test_excludeuchars += char
+
+        password_object = PasswordGenerator(minlen=test_minlen,
+            maxlen=test_maxlen, minuchars=test_minuchars,
+            minlchars=test_minlchars, minnumbers=test_minnumbers,
+            minschars=test_minschars, excludeuchars=test_excludeuchars,
+            excludelchars=test_excludelchars,
+            excludenumbers=test_excludenumbers,
+            excludeschars=test_excludeschars)
+
+        password = password_object.generate()
+
+        self.assertLessEqual(test_minlen, len(password))
+
+        self.assertLessEqual(len(password), test_maxlen)
+
+        lcount = 0
+        ucount = 0
+        numcount = 0
+        scount = 0
+
+        for char in password:
+            self.assertNotIn(char, test_excludelchars)
+            self.assertNotIn(char, test_excludeuchars)
+            self.assertNotIn(char, test_excludenumbers)
+            self.assertNotIn(char, test_excludeschars)
+
+            if char in string.ascii_lowercase:
+                lcount += 1
+
+            elif char in string.ascii_uppercase:
+                ucount += 1
+
+            elif char in string.digits:
+                numcount += 1
+
+            elif char in special_chars:
+                scount += 1
+
+        self.assertLessEqual(test_minlchars, lcount)
+        self.assertLessEqual(test_minuchars, ucount)
+        self.assertLessEqual(test_minnumbers, numcount)
+        self.assertLessEqual(test_minschars, scount)
+
+    def test_quit(self):
+        test_client = FlaskClient(create_read.app)
+        response = test_client.post("/display-page",
+            data={"username": "test_username", "password": "test_pass"})
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
